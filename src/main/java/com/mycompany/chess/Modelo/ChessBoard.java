ee02b877dbeee6bd4a998cd437c2351a3a7a34a7
JuanK <89666771+juankchb@users.noreply.github.com>
2024-02-12 14:52:31 -0500

    Create ChessBoard.java

---
+++ src/main/java/com/mycompany/chess/Modelo/ChessBoard.java
@@ -0,0 +1,13 @@
+public class ChessBoard {
+  Square[][] square=new square[8][8];
+
+  public void initBoard(){
+  }
+
+  public void displayBoard(){
+  }
+
+  publid void updateBoard(){
+  }
+  
+}
