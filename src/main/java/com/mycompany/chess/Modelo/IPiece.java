e392c0f87326cf5010520bbad0ceb07de7eec9d8
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2024-02-12 20:17:30 +0000

    elimino clases

---
+++ src/main/java/com/mycompany/chess/Modelo/IPiece.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public abstract class IPiece {
+
+    Position coords;
+    string color;
+    
+    public IPiece(int row,int col,string color){
+        this.coords = new Position(row,col);
+        this.color = color;
+    }
+
+    public abstract void validMove();
+    public abstract ArrayList<Position> getValidMoves();
+
+
+}
