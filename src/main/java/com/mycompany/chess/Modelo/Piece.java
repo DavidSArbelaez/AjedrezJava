73a621a9f495b133b85c6a7351e374c887362dfb
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2024-02-12 19:44:50 +0000

    Agrego dos clases

--- src/main/java/com/mycompany/chess/Modelo/Piece.java
+++ src/main/java/com/mycompany/chess/Modelo/Piece.java
@@ -1,2 +1,33 @@
 package com.mycompany.chess.Modelo;
 
+import java.util.ArrayList;
+
+public abstract class Piece {
+    private String color;
+    private Square currentPosition;
+
+    public Piece(String color, Square currentPosition) {
+        this.color = color;
+        this.currentPosition = currentPosition;
+    }
+
+    public abstract boolean moveTo(Square square);
+
+    public abstract ArrayList<Square> getValidMoves();
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Square getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(Square currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+}
