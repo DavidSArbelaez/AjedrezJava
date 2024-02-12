3971afb1061f2213f8966949b54890682e365526
JuanK <89666771+juankchb@users.noreply.github.com>
2024-02-12 14:45:53 -0500

    Create Square.java

---
+++ src/main/java/com/mycompany/chess/Modelo/Square.java
@@ -0,0 +1,27 @@
+package com.mycompany.chess.Modelo;
+
+public class Square {
+    int row;
+    int column;
+    
+  // Getter
+  public int getRow() {
+    return row;
+  }
+
+  // Setter
+  public void setRow(int newRow) {
+    this.row = newRow;
+  }
+
+  // Getter
+  public int getColumn() {
+    return column;
+  }
+
+  // Setter
+  public void setColumn(int newColumn) {
+    this.row = newColumn;
+  }
+
+}
