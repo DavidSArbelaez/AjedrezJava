e557c6d613b3daad9b49f32dd2d0c6142a6c9a39
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2024-02-14 17:30:14 +0000

    elimino clases

--- src/main/java/com/mycompany/chess/Modelo/Pawn.java
+++ src/main/java/com/mycompany/chess/Modelo/Pawn.java
@@ -1,7 +1,17 @@
 public class Pawn implements IPiece{
     public Boolean validMove(Position pos){
+        
+        
         return true;
     }
+
+    /*
+     * La idea es verificar si se puede realizar la captura al paso,para accionar asi
+     */
+    public Boolean passant(){
+        return true;
+    }
+
     public ArrayList<Square> getValidMoves(){
 
         return null;
