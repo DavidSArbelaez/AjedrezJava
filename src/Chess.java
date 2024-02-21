03029ebb95546eb9aaa637123f194d106975a4e2
JuanK <89666771+juankchb@users.noreply.github.com>
2024-02-20 20:01:44 -0500

    Update Chess.java

--- src/Chess.java
+++ src/Chess.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
+import javax.swing.*;
 
-package com.mycompany.chess;
-
-/**
- *
- * @author Labing
- */
 public class Chess {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SwingUtilities.invokeLater(() -> {
+            Vista chessBoardView = new Vista();
+            chessBoardView.setVisible(true);
+        });
     }
 }
