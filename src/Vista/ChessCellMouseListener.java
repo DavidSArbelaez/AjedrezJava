d506fe8501247f5e095039c414895802732fa5a5
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2024-03-04 13:22:45 -0500

    Update ChessCellMouseListener.java

--- src/Vista/ChessCellMouseListener.java
+++ src/Vista/ChessCellMouseListener.java
@@ -6,15 +6,37 @@ import java.awt.event.MouseEvent;
 public class ChessCellMouseListener extends MouseAdapter {
     private int row;
     private int col;
-
+    public boolean mover;
     public ChessCellMouseListener(int row, int col) {
         this.row = row;
         this.col = col;
+        this.mover=false;
     }
 
     @Override
     public void mouseClicked(MouseEvent e) {
         // Aquí puedes agregar la lógica para manejar los clics en el tablero
-        System.out.println("Clic en la casilla: (" + row + ", " + col + ")");
+    	this.mover=true;
+    	System.out.println("Clic en la casilla: (" + row + ", " + col + ")");
     }
+
+	public int getRow() {
+		return row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public boolean isMover() {
+		return mover;
+	}
+
+	public void resetMover() {
+		this.mover = false;
+	}
+	
+	
+    
+    
 }
