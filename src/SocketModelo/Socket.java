c42c60039f5f352cf28cb521bb4e805db47d5952
JuanK <89666771+juankchb@users.noreply.github.com>
2024-03-04 15:32:58 -0500

    Update Socket.java

--- src/SocketModelo/Socket.java
+++ src/SocketModelo/Socket.java
@@ -1,1 +1,18 @@
+import java.io.*;
 
+public class Socket {
+        // Función para serializar un String[][]
+    public static byte[] serializarStringArray(String[][] arreglo) {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+            
+            oos.writeObject(arreglo);
+            return bos.toByteArray();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Aquí puedes manejar la excepción según tus necesidades
+            return null;
+        }
+    }
+}
