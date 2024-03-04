98f1b5f043f064104ceb116b71b8864f78c6c4f5
JuanK <89666771+juankchb@users.noreply.github.com>
2024-03-04 15:38:21 -0500

    Update Server.java

--- src/SocketModelo/Server.java
+++ src/SocketModelo/Server.java
@@ -1,1 +1,28 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    private static final int PUERTO = 12345;
+
+    public static void main(String[] args) {
+        try {
+            // Abre un ServerSocket en el puerto especificado
+            ServerSocket serverSocket = new ServerSocket(PUERTO);
+            System.out.println("Servidor escuchando en el puerto " + PUERTO);
+
+            while (true) {
+                // Acepta la conexión entrante
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado desde " + clientSocket.getInetAddress());
+
+                //Creando un hilo para cada cliente
+                new Thread(new ManejadorCliente(clientSocket)).start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error al abrir el ServerSocket: " + e.getMessage());
+        }
+    }
+}
