00f787095400c3cecf571537e0eb5d5274fe08d0
DavidSArbelaez <108475569+DavidSArbelaez@users.noreply.github.com>
2024-03-11 15:16:32 -0500

    Update Cliente.java

--- src/SocketModelo/Cliente.java
+++ src/SocketModelo/Cliente.java
@@ -9,6 +9,7 @@ public class Cliente {
     private Socket socket;
     private BufferedReader in;
     private PrintWriter out;
+    boolean MsgSend, MsgRec;
 
     public Cliente(String host_ip, int port) {
         this.server_IP = host_ip;
@@ -16,12 +17,13 @@ public class Cliente {
         System.out.println(server_IP + " " + this.port);
         try {
             this.socket = new Socket(host_ip, this.port);
-            // Crear flujos de entrada y salida para la comunicación con el servidor
             this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.out = new PrintWriter(socket.getOutputStream(), true);
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
+        MsgSend = false;
+        MsgRec = false;
     }
 
     public void sendDataToServer(String message) {
@@ -42,9 +44,8 @@ public class Cliente {
                 this.out = new PrintWriter(socket.getOutputStream(), true);
             }
             
-            System.out.println("Entro a recibir el mensaje");
             String message = in.readLine();
-            System.out.println("Servidor: " + message);
+            System.out.println("Entro a recibir el mensaje");
             return message;
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
@@ -55,9 +56,12 @@ public class Cliente {
     public void close() {
         try {
             // Cerrar conexiones
-            if (in != null) in.close();
-            if (out != null) out.close();
-            if (socket != null) socket.close();
+            if (in != null)
+                in.close();
+            if (out != null)
+                out.close();
+            if (socket != null)
+                socket.close();
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
