5de7ef343f29e9bcfa51578b5f720c3183ab5f6d
JuanK <89666771+juankchb@users.noreply.github.com>
2024-03-19 12:16:31 -0500

    Update and rename MyServlet.java to ControladorServlet.java

---
+++ src/Controlador/ControladorServlet.java
@@ -0,0 +1,32 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Model;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/chessLeyva")
+public class ControladorServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public ControladorServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        // Llama al método estático de HTMLHelper para generar el contenido HTML
+        ServletHelper.generarContenidoHTML(out);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
